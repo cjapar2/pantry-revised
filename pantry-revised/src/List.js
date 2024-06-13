65a248134d9145c02bf7e5e3204b1199c5c3c195
CJ Apar <cjapar2@gmail.com>
2024-06-12 23:10:30 -0700

    Added a few components for the basic structure of app.

---
+++ pantry-revised/src/List.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './List.css';
+
+export function List() {
+  return (
+    <div className="ListContainer">
+        <h1 className="ListTitle">hi</h1>
+    </div>
+  )
+}
