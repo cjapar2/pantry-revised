6b83afc37cea40d6f7549ea6b0359f14d2455124
CJ Apar <cjapar2@gmail.com>
2024-06-14 00:09:43 -0700

    Changed file structure to be more organized. Also added a little bit of css styling. Hopefully its coherent idk we'll see. Oh and need to make an additem button.

---
+++ pantry-revised/src/components/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/List.css';
+
+export function List() {
+  return (
+    <div className="ListContainer">
+        <h1 className="ListTitle">Fridge</h1>
+        <button className="AddItem"></button>
+    </div>
+  )
+}
