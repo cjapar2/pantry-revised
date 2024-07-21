05c764f675264f5d20ddc3c61effa49cb1aefd44
CJ <cjapar2@gmail.com>
2024-07-21 16:21:39 -0700

    Added a button to sort the item listings alphabetically (ascending and descending). I'm not sure if it updates correctly. So for a TODO: create a trash button on each item listing that shows only when you hover over it.

--- pantry-revised/src/components/List.js
+++ pantry-revised/src/components/List.js
@@ -5,19 +5,49 @@ import { faSquarePlus } from '@fortawesome/free-regular-svg-icons';
 import AddItemForm from './AddItemForm';
 import Modal from '@mui/material/Modal';
 import ItemListing from './ItemListing';
+import { Button } from '@mui/material';
+import { ArrowDownward, ArrowUpward, SwapVert } from '@mui/icons-material';
 // import { LeftDrawer } from './LeftDrawer';
 
 export function List() {
+  const initialItems = JSON.parse(localStorage.getItem('items')) || [];
 
+  // State to show addItemForm
+  const [showAddForm, setShowAddForm] = useState(false);
+
+  // States to reorder list
+  const [sortOrder, setsortOrder] = useState('default');
+
+  // Function to sort items by name
+  function sortItems(sortOrder) {
+    let sortedItems;
+    switch (sortOrder) {
+      case 'ascending':
+        console.log('items:', items);
+        sortedItems = [...items].sort((a, b) => a.itemName.localeCompare(b.itemName));
+        break;
+      case 'descending':
+        sortedItems = [...items].sort((a, b) => b.itemName.localeCompare(a.itemName));
+        break;
+      case 'default':
+        sortedItems = initialItems;
+    }
+    setItems(sortedItems);
+  }
+
+  function handleNameSort() {
+    const nextSortOrder = sortOrder === 'default' ? 'ascending' : sortOrder === 'ascending' ? 'descending' : 'default';
+    setsortOrder(nextSortOrder);
+    sortItems(nextSortOrder);
+  }
+
+  // State to show items from localStroage
   const [items, setItems] = useState(() => {
     // Initialize state from localStorage
     const storedItems = JSON.parse(localStorage.getItem('items'));
     console.log('Initializing items from localStorage:', storedItems); // Log initialization
     return storedItems || [];
   });
-  
-  // State to show addItemForm
-  const [showAddForm, setShowAddForm] = useState(false);
 
   // Store items in localStorage whenever items change
   useEffect(() => {
@@ -26,7 +56,7 @@ export function List() {
   }, [items]);
 
   // Add itemlisting to list of itemlistings
-  const addItem = (itemObj) => {
+  function addItem(itemObj) {
     setItems((prevItems) => [...prevItems, itemObj]);
   };
 
@@ -37,18 +67,25 @@ export function List() {
         <FontAwesomeIcon icon={faSquarePlus} size="2xl" className="OpenAddFormBtn"
           onClick={() => setShowAddForm(true)}
         />
+        {/* Modal that shows form to add items depending on button press */}
         <Modal open={showAddForm} onClose={() => setShowAddForm(false)}>
           <AddItemForm addItem={addItem}/>
         </Modal>
         
-        {/* List Labels */}
-        {/* <div className='labelsContainer'>
-          <div className='labels'>
-            <div>Name</div>
-            <div>Amount</div>
-            <div>Comments</div>
-          </div>
-        </div> */}
+        {/* Container that contains labels */}
+        <div className='labelsContainer'>
+          {/* Buttons for sorting list */}
+          <Button className='nameLabel'
+            variant="contained" color="primary"
+            onClick={handleNameSort}
+            endIcon={
+              sortOrder === 'default' ? <ArrowDownward /> :
+              sortOrder === 'ascending' ? <ArrowUpward /> : <SwapVert />
+            }
+          >
+            {sortOrder}
+          </Button>
+        </div>
         
         {/* Container that contains item listings */}
         <div className='listingsContainer'>
