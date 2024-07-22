f177f83f9b5e106ed18faa8654882e9c62a49d91
cjaapr2 <cjapar2@gmail.com>
2024-07-22 15:04:43 -0700

    Fixed styling for button. TODO: Make it so when hovering over item listing, make button appear. Make button delete item listing from local storage.

--- pantry-revised/src/components/ItemListing.js
+++ pantry-revised/src/components/ItemListing.js
@@ -6,8 +6,8 @@ import { IconButton } from '@mui/material';
 export default function ItemListing({itemName, amount, dateAdded, comment}) {
   return (
     <div className='itemContainer'>
-      <IconButton size='small'>
-        <DeleteIcon className='deleteButton'/>
+      <IconButton className='delete-button' size='small'>
+        <DeleteIcon />
       </IconButton>
       <div className='name'>{itemName}</div>
       <div className='amount'>{amount}</div>
