5d149d0174b567f7009bb7ddb1126b14bacc6746
cjaapr2 <cjapar2@gmail.com>
2024-08-21 14:04:25 -0700

    Postman can now PUT/UPDATE specified lists in the database.

--- server/index.js
+++ server/index.js
@@ -14,15 +14,41 @@ app.post('/lists', async(req, res) => {
         console.log(req.body);
         const { name } = req.body;
         const newList =  await pool.query(
-            "INSERT INTO lists (name) VALUES($1)",
+            "INSERT INTO lists (name) VALUES($1) RETURNING *",
             [name]
         );
 
-        res.json(newList);
+        res.json(newList.rows[0]);
     } catch (err) {
         console.error(err.message);
     }
 })
+
+// get all lists
+app.get('/lists', async(req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM lists");
+        res.json(allTodos.rows);
+    } catch {
+        console.error(err.message);
+    }
+});
+
+// update a list
+app.put('/lists/:list_id', async(req, res) => {
+    try {
+        const { list_id } = req.params;
+        const { name } = req.body;
+        const updateList = await pool.query(
+            "UPDATE lists SET name = $1 WHERE list_id = $2",
+            [name, list_id]
+        );
+        res.json('List updated');
+
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 app.listen(5000, () => {
     console.log('Server has started on port 5000.');
